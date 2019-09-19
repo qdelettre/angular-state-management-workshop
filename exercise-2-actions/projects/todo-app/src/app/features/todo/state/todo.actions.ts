618c6e6fd32f6c3fa2052ee78cd25ee860113be9
tomastrajan <tomas.trajan@gmail.com>
2019-09-19 09:33:26 +0200

    fix(exercise-2-actions): improve todo comments

--- exercise-2-actions/projects/todo-app/src/app/features/todo/state/todo.actions.ts
+++ exercise-2-actions/projects/todo-app/src/app/features/todo/state/todo.actions.ts
@@ -5,30 +5,32 @@ import { Todo, TodoFilter } from './todo.model';
 // every action has a type, don't forget to use following type format: [<action-origin>] <action-description>
 // eg: [User Page] Load Users
 
+// try to explore reducer to see how is given action used to produce a new state
+
 // TODO 1: create "addTodo" action with a "title" payload (the text of the todo)
 export const addTodo = undefined;
 
-// TODO 2: create "toggleTodo" action with a "id" payload (uuid so string)
+// TODO 2: create "toggleTodo" action with a "id" payload (string)
 export const toggleTodo = undefined;
 
-// TODO 3: create "removeTodo" action with a "id" payload (uuid so string)
+// TODO 3: create "removeTodo" action with a "id" payload (string)
 export const removeTodo = undefined;
 
-// TODO 4: create "editTodo" action with a "id" payload (uuid so string), sets "editedTodoId" in state
+// TODO 4: create "editTodo" action with a "id" payload (string)
 export const editTodo = undefined;
 
-// TODO 5: create "cancelEditTodo" action without payload, sets "editedTodoId" to null in state
+// TODO 5: create "cancelEditTodo" action without payload
 export const cancelEditTodo = undefined;
 
-// TODO 6: create "updateTodo" action with a "todo" payload (Todo type)
+// TODO 6: create "updateTodo" action with a "todo" payload (Todo type), (flat or in property? explore reducer...)
 export const updateTodo = undefined;
 
 // TODO 7: create "removeDoneTodos" action without payload
 export const removeDoneTodos = undefined;
 
-// TODO 7: create "filterTodos" action  with a "filter" payload (TodoFilter type)
+// TODO 8: create "filterTodos" action  with a "filter" payload (TodoFilter type)
 export const filterTodos = undefined;
 
-// TODO 8: create "addTodoWithId" action using CUSTOM action factory method (prop) => payload
+// TODO 9: create "addTodoWithId" action using CUSTOM action factory method (prop) => payload
 // the factory with accept title (text of the to-do) but the resulting payload will also contain generated "id" (using uuid)
 export const addTodoWithId = undefined;
