d4d077f66cb92a3998bd27a23d9d3eef2ceed1a6
tomastrajan <tomas.trajan@gmail.com>
2021-12-03 17:11:04 +0100

    fix(all):  remove tslint config, add eslint root config, adjust routerReducer export

--- exercise-3-reducers/projects/todo-app/src/app/core/router-state/state/router.reducer.ts
+++ exercise-3-reducers/projects/todo-app/src/app/core/router-state/state/router.reducer.ts
@@ -1,9 +1,10 @@
 import { RouterReducerState, routerReducer } from '@ngrx/router-store';
 
+export { routerReducer } from '@ngrx/router-store';
+
 export const routerFeatureKey = 'router';
 
 export interface State {
   router: RouterReducerState<any>;
 }
 
-export { routerReducer };
