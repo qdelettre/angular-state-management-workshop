645033dbaefe16dfd9ace417ccd2bf6ffafad9d7
tomastrajan <tomas.trajan@gmail.com>
2021-12-04 18:34:16 +0100

    feat(exercise-5-testing): task description

--- exercise-5-testing/projects/todo-app/src/app/features/todo/state/todo.selectors.spec.ts
+++ exercise-5-testing/projects/todo-app/src/app/features/todo/state/todo.selectors.spec.ts
@@ -5,10 +5,11 @@ describe('Todo selectors', () => {
   describe('selectTodosView', () => {
     it('returns todo view state with ALL filter', () => {
       // TODO 4: implement "selectTodos" selector test (remember that selector exposes its function in "projector" property)
-      // test if result contains desired data (eg how many todos were selected based on provided params)
+      // test if result contains desired data (eg how many todos were selected based on provided state)
       // projector might not get proper IDE type support depending on editor
       // in that case, please check what you pass into selector in its implementation to know what test data you have to create
       // feel free to use "initialState" for all the cases when you need to pass in feature state
+      // and adjust it if you want to override some property, eg { ...initialState, todoFilter: 'DONE' }
     });
 
     // TODO 5: implement all selector tests
