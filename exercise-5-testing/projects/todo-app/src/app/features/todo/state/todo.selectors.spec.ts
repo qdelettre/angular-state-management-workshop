456bc60f3517e5c170feaa2d7a280b4f1647e38b
tomastrajan <tomas.trajan@gmail.com>
2021-12-04 18:14:03 +0100

    fix(exercise-5-testing): use view selector pattern for todo

--- exercise-5-testing/projects/todo-app/src/app/features/todo/state/todo.selectors.spec.ts
+++ exercise-5-testing/projects/todo-app/src/app/features/todo/state/todo.selectors.spec.ts
@@ -1,14 +1,9 @@
-import {
-  selectEditedTodo,
-  selectTodoFilter,
-  selectTodos,
-  selectTodosCount
-} from './todo.selectors';
+import { selectTodosView } from './todo.selectors';
 import { initialState } from './todo.reducer';
 
 describe('Todo selectors', () => {
-  describe('selectTodos', () => {
-    it('returns all todos with ALL filter', () => {
+  describe('selectTodosView', () => {
+    it('returns todo view state with ALL filter', () => {
       // TODO 4: implement "selectTodos" selector test (remember that selector exposes its function in "projector" property)
       // test if result contains desired data (eg how many todos were selected based on provided params)
       // projector might not get proper IDE type support depending on editor
@@ -16,22 +11,9 @@ describe('Todo selectors', () => {
       // feel free to use "initialState" for all the cases when you need to pass in feature state
     });
 
-    it('returns done todos with DONE filter', () => {
-      // TODO 5: implement all selector tests
-    });
+    // TODO 5: implement all selector tests
+    it('returns done todos with DONE filter', () => {});
 
     it('returns active todos with ACTIVE filter', () => {});
-  });
-
-  describe('selectTodosCount', () => {
-    it('returns count of todos', () => {});
-  });
-
-  describe('selectTodoFilter', () => {
-    it('returns filter value', () => {});
-  });
-
-  describe('selectEditedTodo', () => {
-    it('returns edited todo object', () => {});
   });
 });
