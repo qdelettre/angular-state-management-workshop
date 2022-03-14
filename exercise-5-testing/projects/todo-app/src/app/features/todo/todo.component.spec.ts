5813c92acbe8515cf09738d66d442b9de98860d7
tomastrajan <tomas.trajan@gmail.com>
2022-03-14 15:26:15 +0100

    feat(exercise-5-schematics): improve task descriptions

--- exercise-5-testing/projects/todo-app/src/app/features/todo/todo.component.spec.ts
+++ exercise-5-testing/projects/todo-app/src/app/features/todo/todo.component.spec.ts
@@ -59,19 +59,28 @@ describe('TodoComponent', () => {
     // the selector variable will be of type based on what it should return
     // eg "MemoizedSelector<State, Partial<ReturnType<typeof selectTodosView>>>" for mockSelectTodoView
     // the mock data should contain only data actually used in the components view
+    // provide at least one mock todo item ("items" vs "todos" which one is correct?) and todoFilter value
+
+    // once the mock selector was prepared, call "fixture.detectChanges()"
   });
 
   it('should render todo correct todo items', () => {
     // TODO 9: implement all todo component tests
     // use helper functions like "getTodoItems()" (find them at the top of the test) to check if component rendered the state correctly
     // eg: correct count of rendered todo items, todo item text, correct filter description based on filter value, ...
-    // dont forget to call "fixture.detectChanges()" at the beginning of each test(or after overriding of the selector data) to trigger component rendering
   });
 
   it('displays correct description based on filter: "DONE"', () => {
     // some tests need other data than provided in the "beforeEach" with "overrideSelector"
     // in that case we can set new data in previously overridden selector (eg "mockSelectTodosView")
     // using the "setResult()" method followed by the "store.refreshState()" and "fixture.detectChanges()"
+
+    // SIDE NOTE: OnPush and multiple fixture.detectChanges()
+    // please note that even though our component uses "OnPush" change detection strategy,
+    // we are able to call "fixture.detectChanges()" more than once! (in before each and in the test)
+    // the reason for that is that component uses "| async" pipe to consume data from the store
+    // so the component will be "marked for check" when we push new value tough the mock selector using "refreshState()"
+    // this is yet another benefit of using NgRx!
   });
 
   it('displays correct description based on filter: "ACTIVE"', () => {
